31be07034608387bb8bd5c70c1478c3bc07e5a7b
0minKoh <esesang13@naver.com>
2022-03-09 21:04:24 +0900

    js 선언 및 버그 개선

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-const badgeEl = document.querySelector('.background1 .badge')
+'use strict'
+
+// 주의할 점
+// 1. html에서 js를 연결
+// 2. html에서 js 라이브러리 연결 후 js를 연결할때, defer을 활용하여 먼저 해석
+// 3. console.log로 오류 확인 가능
+
+const badgeEl = document.querySelector('.badges')
+console.log(badgeEl)
 window.addEventListener('scroll', _.throttle(function () {
   if (window.scrollY > 500) {
-    // gsap.to(요소, 지속시간, 옵션);
+    // gsap.to(요소, 지속시간, {옵션})
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
     })
+    console.log('사라짐')
   } else {
-    gsap.to(badgeEl, .6, {
+    gsap.to(badgeEl, .2, {
       opacity: 1,
       display: 'block'
     })
